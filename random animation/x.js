5796dba010b276416c4e528df4e4ef68d8f83156
George Jay <leriaz@n0where.eu>
2016-06-10 13:46:15 +0300

    Update x.js

--- random animation/x.js
+++ random animation/x.js
@@ -1,10 +1,3 @@
-This text was downloaded from Beetxt.com
-Beetxt.com is the quickest tool to save text online.
-
-This file: http://beetxt.com/dNO
-Created: 2016-03-09
--------------------------
-
 window.onload = loadScene;
 
 var canvas, gl,
